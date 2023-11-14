64c2b8805b4ff0fca3a204f86ccd1a93e6f2fb0a
Felipe Fernandez H <felipefelizx@gmail.com>
2023-11-14 20:34:48 -0300

    Refactorizacion - Arreglo del codigo

--- Backend/index.js
+++ Backend/index.js
@@ -1,102 +1,111 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
-let textAlignment = 'left'; // Variable global para almacenar la alineación del texto
-let textColor = 'black'; // Variable global para almacenar el color del texto
-let textBackgroundColor = 'white'; // Variable global para almacenar el color de fondo del texto
+
+// Configuraciones centralizadas
+const config = {
+  textAlignment: "left",
+  textColor: "black",
+  textBackgroundColor: "white",
+};
 
 app.use(cors());
 app.use(express.json());
 
-// Rutas de formato
-app.post('/api/changeTextAlignment', (req, res) => {
+// Middleware para manejar solicitudes no encontradas
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
+
+// Middleware para manejar errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: "Something went wrong!" });
+});
+
+// Alinear texto
+app.post("/api/text/alignment", (req, res) => {
   const { alignment } = req.body;
-  console.log('Texto justificado:', alignment);
-
-  // Actualizar la variable global con la nueva alineación
-  textAlignment = alignment;
-
-  res.json({ success: true, message: 'Justificación del texto cambiada exitosamente' });
+  console.log("Texto justificado:", alignment);
+  // Actualizar la configuración con la nueva alineación
+  config.textAlignment = alignment;
+  res.json({
+    success: true,
+    message: "Justificación del texto cambiada exitosamente",
+    data: { alignment: config.textAlignment },
+  });
 });
 
-app.get('/api/getTextAlignment', (req, res) => {
-  // Devolver la alineación actual almacenada en la variable global
-  res.json({ alignment: textAlignment });
+app.get("/api/text/alignment", (req, res) => {
+  // Devolver la alineación actual almacenada en la configuración
+  res.json({ alignment: config.textAlignment });
 });
-
-
-
-
-app.post('/api/changeTextColor', (req, res) => {
-  const { textColor } = req.body;
-  console.log('Texto con nuevo color:', textColor);
-  res.json({ success: true, message: 'Color del texto cambiado exitosamente' });
-});
-
-app.post('/api/changeTextBackgroundColor', (req, res) => {
-  const { backgroundColor } = req.body;
-  console.log('Texto con nuevo color de fondo:', backgroundColor);
-  res.json({ success: true, message: 'Color de fondo del texto cambiado exitosamente' });
-});
-
 
 // Texto en negrita
-app.post('/api/changeTextBold', (req, res) => {
+app.post("/api/text/bold", (req, res) => {
   const { bold } = req.body;
-  console.log('Texto en negrita:', bold);
-  res.json({ success: true, message: 'Texto en negrita cambiado exitosamente', bold: bold });
+  // Lógica para cambiar el texto en negrita en el backend
+  console.log("Texto en negrita:", bold);
+  res.json({
+    success: true,
+    message: "Texto en negrita cambiado exitosamente",
+  });
 });
 
 // Texto en cursiva
-app.post('/api/changeTextItalic', (req, res) => {
+app.post("/api/text/italic", (req, res) => {
   const { italic } = req.body;
-  console.log('Texto en cursiva:', italic);
-  res.json({ success: true, message: 'Texto en cursiva cambiado exitosamente', italic: italic });
-});
-
-// texto en subrayado
-app.post('/api/changeTextUnderline', (req, res) => {
-  const { underline } = req.body;
-  console.log('Texto subrayado:', underline);
-  res.json({ success: true, message: 'Texto subrayado cambiado exitosamente', underline: underline });
-});
-
-
-// Rutas de tamaño de letra
-app.route('/api/changeTextSize')
-  .get((req, res) => {
-    // Manejar la lógica para solicitudes GET (si es necesario)
-    res.status(404).send('Not Found');
-  })
-  .post((req, res) => {
-    const { size } = req.body;
-    // Lógica para cambiar el tamaño de la letra en el backend
-    console.log('Texto con nuevo tamaño:', size);
-    res.json({ success: true, message: 'Tamaño de la letra cambiado exitosamente' });
+  console.log("Texto en cursiva:", italic);
+  res.json({
+    success: true,
+    message: "Texto en cursiva cambiado exitosamente",
+    italic: italic,
   });
-
-
-app.post('/api/changeTextFont', (req, res) => {
-  const { font } = req.body;
-  console.log('Texto con nuevo tipo de letra:', font);
-  res.json({ success: true, message: 'Tipo de letra cambiado exitosamente' });
 });
 
-app.post('/api/changeTextLineHeight', (req, res) => {
-  const { lineHeight } = req.body;
-  console.log('Texto con nuevo interlineado:', lineHeight);
-  res.json({ success: true, message: 'Interlineado cambiado exitosamente' });
+// Texto subrayado
+app.post("/api/text/underline", (req, res) => {
+  const { underline } = req.body;
+  console.log("Texto subrayado:", underline);
+  res.json({
+    success: true,
+    message: "Texto subrayado cambiado exitosamente",
+    underline: underline,
+  });
 });
 
-app.post('/api/changeTextLetterSpacing', (req, res) => {
-  const { letterSpacing } = req.body;
-  console.log('Texto con nuevo espaciado entre letras:', letterSpacing);
-  res.json({ success: true, message: 'Espaciado entre letras cambiado exitosamente' });
+app.post("/api/text/color", (req, res) => {
+  const { textColor } = req.body;
+  console.log("Texto con nuevo color:", textColor);
+  res.json({
+    success: true,
+    message: "Color del texto cambiado exitosamente",
+  });
 });
 
-app.get('/', (req, res) => {
-  res.send('Hola desde el backend');
+app.post("/api/text/background-color", (req, res) => {
+  const { backgroundColor } = req.body;
+  console.log("Texto con nuevo color de fondo:", backgroundColor);
+  res.json({
+    success: true,
+    message: "Color de fondo del texto cambiado exitosamente",
+  });
+});
+
+// Tamaño de letra
+app.post("/api/text/size", (req, res) => {
+  const { size } = req.body;
+  // Lógica para cambiar el tamaño de la letra en el backend
+  console.log("Texto con nuevo tamaño:", size);
+  res.json({
+    success: true,
+    message: "Tamaño de la letra cambiado exitosamente",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hola desde el backend");
 });
 
 app.listen(PORT, () => {
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,45 +1,17 @@
 import React, { useState } from "react";
-import {
-  FaAlignLeft,
-  FaAlignCenter,
-  FaAlignRight,
-  FaBold,
-  FaItalic,
-  FaUnderline,
-} from "react-icons/fa";
-import { ImLink } from "react-icons/im";
-import "./App.css";
-
-function TableComponent({ rows, columns }) {
-  // Lógica para generar la tabla según las filas y columnas
-  return <table>{/* Generar contenido de la tabla aquí */}</table>;
-}
-
-function ImageComponent({ file }) {
-  return <img src={URL.createObjectURL(file)} alt="Uploaded" />;
-}
+import {FaAlignLeft,FaAlignCenter,FaAlignRight,FaBold,FaItalic,FaUnderline} from "react-icons/fa";
 
 function App() {
-  const [tableDimensions, setTableDimensions] = useState({
-    rows: 0,
-    columns: 0,
-  });
   const [fontColor, setFontColor] = useState("#3c8dbc");
   const [backColor, setBackColor] = useState("orange");
   const [fontSize, setFontSize] = useState("12px");
-  const [file, setFile] = useState(null);
   const [paragraphs, setParagraphs] = useState([{ id: 0, content: "" }]);
-  const [showContentInConsole, setShowContentInConsole] = useState(false);
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
   const [isUnderlined, setIsUnderlined] = useState(false);
   const [alignment, setAlignment] = useState("left");
 
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-  };
-
+  // Funcion para aplicar estilos al texto
   const applyBackendStyle = async (endpoint, data) => {
     try {
       const response = await fetch(`http://localhost:5000/api/${endpoint}`, {
@@ -53,7 +25,6 @@ function App() {
       if (!response.ok) {
         throw new Error(`Error al aplicar el estilo: ${response.statusText}`);
       }
-
       const result = await response.json();
       console.log(result.message);
       console.log(result);
@@ -62,23 +33,14 @@ function App() {
     }
   };
 
-  const handleFontColorClick = () => {
-    const color = prompt("Introduce el color de la fuente:", "#3c8dbc");
-    setFontColor(color);
-    applyBackendStyle("changeTextColor", { textColor: color });
+  // Funcion para aplicar estilos al texto
+  const applyStyleToContentEditable = (style, value) => {
+    document.execCommand("styleWithCSS", null, true);
+    document.execCommand(style, false, value);
+    document.execCommand("styleWithCSS", null, false);
   };
 
-  const handleBackColorClick = () => {
-    const color = prompt("Introduce el color de fondo:", "orange");
-    setBackColor(color);
-    applyBackendStyle("changeTextBackgroundColor", { backgroundColor: color });
-  };
-  const handleFontSizeChange = (e) => {
-    const newSize = e.target.value + "pt";
-    setFontSize(newSize);
-    applyBackendStyle("changeTextSize", { size: newSize });
-  };
-
+  // Funcion para alinear el texto
   const applyTextAlignment = async (newAlignment) => {
     try {
       await applyBackendStyle("changeTextAlignment", {
@@ -89,103 +51,45 @@ function App() {
       console.error("Error al cambiar la alineación:", error.message);
     }
   };
-
-  const applyStyleToContentEditable = (style, value) => {
-    document.execCommand("styleWithCSS", null, true);
-    document.execCommand(style, false, value);
-    document.execCommand("styleWithCSS", null, false);
-  };
-
+  // Funcion para aplicar negrita
   const applyBoldStyle = () => {
     applyBackendStyle("changeTextBold", { bold: !isBold });
     applyStyleToContentEditable("bold");
     setIsBold(!isBold);
   };
-
+  // Funcion para aplicar cursiva
   const applyItalicStyle = () => {
     applyBackendStyle("changeTextItalic", { italic: !isItalic });
     applyStyleToContentEditable("italic");
     setIsItalic(!isItalic);
   };
-
+  // Funcion para aplicar subrayado
   const applyUnderlineStyle = () => {
     applyBackendStyle("changeTextUnderline", { underline: !isUnderlined });
     applyStyleToContentEditable("underline");
     setIsUnderlined(!isUnderlined);
   };
-
-  const handleEnter = (e) => {
-    if (e.key === "Enter") {
-      const currentParagraph = paragraphs[paragraphs.length - 1];
-      const currentLine = currentParagraph.content.split("\n").pop().trim();
-
-      if (currentLine !== "") {
-        const newParagraphId = paragraphs.length;
-        const newParagraph = {
-          id: newParagraphId,
-          content: "",
-        };
-        setParagraphs((prevParagraphs) => [...prevParagraphs, newParagraph]);
-      }
-    }
+  // Funcion para cambiar el color de fuente
+  const handleFontColorClick = () => {
+    const color = prompt("Introduce el color de fuente:", "#3c8dbc");
+    setFontColor(color);
+    applyBackendStyle("changeTextColor", { color: color });
   };
-
-  const handleContentChange = (e, paragraphId) => {
-    const updatedParagraphs = paragraphs.map((paragraph) =>
-      paragraph.id === paragraphId
-        ? { ...paragraph, content: e.target.textContent }
-        : paragraph
-    );
-
-    // Obtener el contenido después del último "Enter"
-    const contentAfterLastEnter = updatedParagraphs
-      .map((paragraph) => paragraph.content)
-      .join("\n")
-      .split("\n")
-      .pop()
-      .trim();
-
-    // Verificar si el contenido después del último "Enter" está vacío
-    if (contentAfterLastEnter === "") {
-      // Si está vacío, agregar un nuevo párrafo
-      const newParagraphId = updatedParagraphs.length;
-      const newParagraph = {
-        id: newParagraphId,
-        content: "",
-      };
-      setParagraphs((prevParagraphs) => [...prevParagraphs, newParagraph]);
-    } else {
-      // Si no está vacío, simplemente actualizar el estado
-      setParagraphs(updatedParagraphs);
-    }
-
-    // Agregar console.log para mostrar ID y contenido
-    updatedParagraphs.forEach((paragraph) => {
-      console.log(`ID ${paragraph.id}: ${paragraph.content}`);
-    });
+  // Funcion para cambiar el color de fondo
+  const handleBackColorClick = () => {
+    const color = prompt("Introduce el color de fondo:", "orange");
+    setBackColor(color);
+    applyBackendStyle("changeTextBackgroundColor", { backgroundColor: color });
   };
-
-  const generarContenido = () => {
-    let contenido = "";
-
-    if (tableDimensions.rows > 0 && tableDimensions.columns > 0) {
-      contenido += `<div>${TableComponent({
-        rows: tableDimensions.rows,
-        columns: tableDimensions.columns,
-      })}</div>`;
-    }
-
-    if (file) {
-      contenido += `<div>${ImageComponent({ file })}</div>`;
-    }
-
-    return contenido;
+  // Funcion para cambiar el tamaño de fuente
+  const handleFontSizeChange = (e) => {
+    const newSize = e.target.value + "pt";
+    setFontSize(newSize);
+    applyBackendStyle("changeTextSize", { size: newSize });
   };
 
   return (
     <div className="App">
-      <input type="file" id="upload" onChange={handleFileChange} hidden />
-
       <div id="parentEditor">
         <div id="painelEditor">
           <button
@@ -267,8 +171,6 @@ function App() {
           id="editor"
           contentEditable={true}
           style={{ textAlign: alignment }}
-          onKeyDown={handleEnter}
-          onInput={(e) => handleContentChange(e, paragraphs.length - 1)}
         ></div>
       </div>
     </div>
