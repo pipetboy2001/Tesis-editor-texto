b9d1d43708d1a25c8e9b9f84c7940a000d3e09f3
Felipe Fernandez H <felipefelizx@gmail.com>
2023-11-28 01:05:03 -0300

    Style - Diseño componente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { FaAlignLeft, FaAlignCenter, FaAlignRight, FaBold,
+import {
+  FaAlignLeft,
+  FaAlignCenter,
+  FaAlignRight,
+  FaBold,
   FaItalic,
-  FaUnderline, } from "react-icons/fa";
+  FaUnderline,
+} from "react-icons/fa";
+import { ChromePicker } from "react-color"; // Importa el componente de paleta de colores
+import "./Styles/Text.css";
 
 function App() {
   const [texts, setTexts] = useState([]);
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
   const [isUnderlined, setIsUnderlined] = useState(false);
+
+  //const heredados
+  const [fontColor, setFontColor] = useState("#3c8dbc");
+  const [backColor, setBackColor] = useState("orange");
+  const [showFontColorPicker, setShowFontColorPicker] = useState(false);
+  const [showBackColorPicker, setShowBackColorPicker] = useState(false);
+  const [currentParagraphNumber, setCurrentParagraphNumber] = useState(1);
+  /* eslint-disable no-unused-vars */
+  const [text, setText] = useState({
+    contenido: "",
+    alineacion: "",
+    bold: false,
+    italic: false,
+    underline: false,
+  });
+  const [fontSize, setFontSize] = useState("16px");
 
   // Funcion para aplicar estilos al texto
   const applyStyleToContentEditable = (style, value) => {
@@ -30,8 +53,8 @@ function App() {
     }
   };
 
-   // Funcion para aplicar negrita
-   const applyBoldStyle = () => {
+  // Funcion para aplicar negrita
+  const applyBoldStyle = () => {
     applyStyleToContentEditable("bold");
     setIsBold(!isBold);
   };
@@ -67,7 +90,9 @@ function App() {
         );
 
         if (!response.ok) {
-          throw new Error(`Error al enviar la actualización: ${response.statusText}`);
+          throw new Error(
+            `Error al enviar la actualización: ${response.statusText}`
+          );
         }
 
         console.log("Actualización enviada con éxito.");
@@ -86,12 +111,79 @@ function App() {
       .catch((error) => console.error("Error al obtener textos: ", error));
   }, []);
 
+  // Función para mostrar marcas de formato en la consola
+  const showFormatMarks = () => {
+    const editor = document.getElementById("editor");
+    if (editor) {
+      const cards = editor.getElementsByClassName("card");
+      let metadata = [];
+
+      Array.from(cards).forEach((card, index) => {
+        const textEditor = card.querySelector("[contenteditable='true']");
+        if (textEditor) {
+          const paragraphs = Array.from(textEditor.childNodes).map((node) => {
+            if (node.nodeType === 3) {
+              return node.textContent.trim();
+            } else if (node.nodeType === 1 && node.nodeName === "DIV") {
+              return node.innerHTML.trim();
+            }
+            return "";
+          });
+
+          const filteredParagraphs = paragraphs.filter(
+            (paragraph) => paragraph !== ""
+          );
+
+          filteredParagraphs.forEach((paragraph, paragraphIndex) => {
+            const currentParagraphObj = {
+              id: index * filteredParagraphs.length + paragraphIndex,
+              content: paragraph,
+              text: "",
+              number: index * filteredParagraphs.length + paragraphIndex + 1,
+            };
+
+            metadata.push(currentParagraphObj);
+          });
+        }
+      });
+
+      setTexts(metadata);
+      console.log(metadata);
+    }
+  };
+
+  // Agrega esta función para manejar el botón de mostrar marcas de formato
+  const handleShowFormatMarks = () => {
+    // Enfoca el editor para que el usuario pueda seguir escribiendo
+    const editor = document.getElementById("editor");
+    if (editor) {
+      showFormatMarks();
+      editor.focus();
+    } else {
+      console.error("Elemento con ID 'editor' no encontrado en el DOM.");
+    }
+  };
+
+  // Función para obtener el texto de un elemento
+  const getInnerText = (element) => {
+    if (element.nodeType === 3) {
+      return element.textContent;
+    } else if (element.nodeType === 1) {
+      let text = "";
+      for (let child of element.childNodes) {
+        text += getInnerText(child);
+      }
+      return text;
+    }
+    return "";
+  };
+
   return (
     <div className="App">
       <div id="parentEditor">
         <div id="editor">
-          {texts.map((text) => (
-            <div key={text._id} className="card">
+          {texts.map((text, index) => (
+            <div key={index} className="card">
               <div id="painelEditor">
                 <button
                   className="btnColor"
@@ -129,18 +221,23 @@ function App() {
                 <button
                   className="btnColor"
                   onClick={applyUnderlineStyle}
-                  style={{ textDecoration: isUnderlined ? "underline" : "none" }}
+                  style={{
+                    textDecoration: isUnderlined ? "underline" : "none",
+                  }}
                 >
                   <FaUnderline />
                 </button>
-
+                <button onClick={handleShowFormatMarks}>¶</button>
               </div>
               <div
                 contentEditable={true}
-                style={{ textAlign: text.alineacion || "left",
-                fontWeight: text.bold ? "bold" : "normal",
-                fontStyle: text.italic ? "italic" : "normal",
-                textDecoration: text.underline ? "underline" : "none",}}
+                className="text-editor"
+                style={{
+                  textAlign: text.alineacion || "left",
+                  fontWeight: text.bold ? "bold" : "normal",
+                  fontStyle: text.italic ? "italic" : "normal",
+                  textDecoration: text.underline ? "underline" : "none",
+                }}
               >
                 {text.contenido}
               </div>
