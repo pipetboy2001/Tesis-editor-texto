a80f5df0f388181cbe230b6b7b1a91cc9d1dc2e9
Felipe Fernandez H <felipefelizx@gmail.com>
2024-01-16 16:35:51 -0300

    Fix - Recarga datos
    utilizar useEffect para llamar a la función fetchData en intervalos regulares. Esto actualizará los datos automáticamente sin necesidad de recargar la página.

--- src/Components/EditorView.js
+++ src/Components/EditorView.js
@@ -1,64 +1,103 @@
-import React, { useState } from "react";
-import TemaDetails from "./TemaDetails";
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
 import { Button, Modal, Form } from "react-bootstrap";
 import AddCircleIcon from "@atlaskit/icon/glyph/add";
+import TemaDetails from "./TemaDetails";
+
+const API_URL = "http://localhost:8000/text";
 
 const EditorView = ({ selectedText }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [newTema, setNewTema] = useState({ tema: "", contenido: "", autor: "" });
+  const [state, setState] = useState({
+    showModal: false,
+    newTema: { tema: "", contenido: "", autor: "" },
+    loading: true,
+    temas: [],
+  });
 
+  const { showModal, newTema, loading, temas } = state;
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${API_URL}/${selectedText._id}`);
+      if (!response.ok) {
+        throw new Error(`Error al obtener temas: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setState((prevState) => ({ ...prevState, temas: data.temas }));
+    } catch (error) {
+      console.error("Error al obtener temas: ", error);
+      // Mostrar mensaje al usuario, por ejemplo, setear un estado de error
+    } finally {
+      setState((prevState) => ({ ...prevState, loading: false }));
+    }
+  };
+
+  const startInterval = () => {
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 30000);
+
+    return () => clearInterval(intervalId);
+  };
+
+  useEffect(() => {
+    if (selectedText && selectedText._id) {
+      fetchData();
+      const intervalCleanup = startInterval();
+      return intervalCleanup;
+    }
+  }, [newTema, selectedText]);
 
   const handleShowModal = () => {
-    setShowModal(true);
+    setState((prevState) => ({ ...prevState, showModal: true }));
   };
 
   const handleCloseModal = () => {
-    setShowModal(false);
-    setNewTema({ tema: "", contenido: "", autor: "" }); // Include the 'autor' property
+    setState((prevState) => ({
+      ...prevState,
+      showModal: false,
+      newTema: { tema: "", contenido: "", autor: "" },
+    }));
   };
-  
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setNewTema((prevTema) => ({ ...prevTema, [name]: value }));
+    setState((prevState) => ({
+      ...prevState,
+      newTema: { ...prevState.newTema, [name]: value },
+    }));
   };
 
   const handleAddTema = async () => {
     try {
-      const response = await fetch(
-        `http://localhost:8000/text/create-tema/${selectedText._id}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            tema: newTema.tema,
-            elementos: [
-              {
-                contenido: newTema.contenido,
-                tipo: "compromiso",
-                alineacion: "left",
-                bold: false,
-                italic: false,
-                underline: false,
-                autor: "Pipet",
-                sentimiento: "positive",
-                orden: 1,
-              },
-            ],
-          }),
-        }
-      );
+      const response = await fetch(`${API_URL}/create-tema/${selectedText._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          tema: newTema.tema,
+          elementos: [
+            {
+              contenido: newTema.contenido,
+              tipo: "compromiso",
+              alineacion: "left",
+              bold: false,
+              italic: false,
+              underline: false,
+              autor: newTema.autor,
+              sentimiento: "positive",
+              orden: 1,
+            },
+          ],
+        }),
+      });
 
       if (!response.ok) {
         throw new Error(`Error al añadir tema: ${response.statusText}`);
       }
 
-      const addedTema = await response.json();
-      // Puedes hacer algo con el tema añadido si es necesario
-
-      // Cierra el modal y actualiza la vista
       handleCloseModal();
     } catch (error) {
       console.error("Error al añadir tema: ", error);
@@ -67,71 +106,73 @@ const EditorView = ({ selectedText }) => {
   };
 
   return (
-    <div className="sticky-header-container">
-    <div className="EditorTexto">
-      {selectedText && selectedText.temas && selectedText.temas.length > 0 ? (
-        selectedText.temas.map((tema) => (
-          <TemaDetails key={tema.id_tema} tema={tema} selectedText={selectedText} />
-        ))
-      ) : (
-        "No hay temas disponibles"
-      )}
-    </div>
-    <Button variant="outline-primary" className="add-tema-button" onClick={handleShowModal}>
-      <AddCircleIcon size="small" />
-      Añadir Tema
-    </Button>
-
-      {/* Modal para añadir un tema */}
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Añadir Tema</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="temaName">
-              <Form.Label>Nombre del Tema</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingrese el nombre del tema"
-                name="tema"
-                value={newTema.tema}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="temaContent">
-              <Form.Label>Contenido del Tema</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={3}
-                placeholder="Ingrese el contenido del tema"
-                name="contenido"
-                value={newTema.contenido}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-            <Form.Group controlId="temaAutor">
-              <Form.Label>Autor</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingrese el autor"
-                name="autor"
-                value={newTema.autor}
-                onChange={handleInputChange}
-              />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Cerrar
-          </Button>
-          <Button variant="primary" onClick={handleAddTema}>
-            Añadir Tema
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
+    <>
+      <div className="sticky-header-container">
+        <div className="EditorTexto">
+          {loading ? (
+            "Cargando temas..."
+          ) : temas.length > 0 ? (
+            temas.map((tema) => (
+              <TemaDetails key={tema.id_tema} tema={tema} selectedText={selectedText} />
+            ))
+          ) : (
+            "No hay temas disponibles"
+          )}
+        </div>
+        <Button variant="outline-primary" className="add-tema-button" onClick={handleShowModal}>
+          <AddCircleIcon size="small" />
+          Añadir Tema
+        </Button>
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Añadir Tema</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="temaName">
+                <Form.Label>Nombre del Tema</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el nombre del tema"
+                  name="tema"
+                  value={newTema.tema}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="temaContent">
+                <Form.Label>Contenido del Tema</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  placeholder="Ingrese el contenido del tema"
+                  name="contenido"
+                  value={newTema.contenido}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+              <Form.Group controlId="temaAutor">
+                <Form.Label>Autor</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el autor"
+                  name="autor"
+                  value={newTema.autor}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Cerrar
+            </Button>
+            <Button variant="primary" onClick={handleAddTema}>
+              Añadir Tema
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </>
   );
 };
 
