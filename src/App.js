6c2dd6030f8283fda2d27736099ff27e936031c6
Felipe Fernandez H <felipefelizx@gmail.com>
2024-01-16 16:44:37 -0300

    Fix - Mejoras de codigo app.js

--- src/App.js
+++ src/App.js
@@ -2,35 +2,46 @@
 import React, { useState, useEffect } from "react";
 import IdSelector from "./Components/IdSelector";
 import Testing from "./Components/Testing";
-
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 const App = () => {
   const [selectedId, setSelectedId] = useState(null);
   const [databaseIds, setDatabaseIds] = useState([]);
-  const [texts, setTexts] = useState([]); // Agrega un estado para almacenar los textos
+  const [texts, setTexts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const apiUrl =
+        process.env.REACT_APP_API_URL || "http://localhost:8000/text";
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error(`Error al obtener textos: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      const ids = data.texts.map((text) => text._id);
+      setDatabaseIds(ids);
+      setTexts(data.texts);
+    } catch (error) {
+      console.error("Error al obtener textos: ", error);
+      // Realiza acciones adicionales en caso de error, como mostrar un mensaje al usuario
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        //const response = await fetch("http://172.111.10.181:8000/text");
-        const response = await fetch("http://localhost:8000/text");
-        if (!response.ok) {
-          throw new Error(`Error al obtener textos: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        const ids = data.texts.map((text) => text._id);
-        setDatabaseIds(ids);
-        setTexts(data.texts); // Almacena los textos en el estado
-      } catch (error) {
-        console.error("Error al obtener textos: ", error);
-      }
-    };
-
+    // Llama a fetchData al montar el componente
     fetchData();
-  }, []);
+
+    // Establece un intervalo para llamar a fetchData cada X milisegundos
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 3000); // Actualiza cada 60 segundos (ajusta según tus necesidades)
+
+    // Limpia el intervalo al desmontar el componente
+    return () => clearInterval(intervalId);
+  }, []); // El segundo parámetro es un array vacío para que se ejecute solo al montar y desmontar
 
   const handleIdSelect = (id) => {
     setSelectedId(id);
@@ -40,25 +51,15 @@ const App = () => {
     <>
       <div className="App">
         {selectedId ? (
-          // Modo de edición con el ID seleccionado
-
-          // Agrega el proveedor de arrastrar y soltar
-          // y pasa el backend de HTML5 como parámetro
-
           <DndProvider backend={HTML5Backend}>
             <Testing
               selectedId={selectedId}
               texts={texts}
-              setTexts={setTexts} // Pasa la función para actualizar los textos
+              setTexts={setTexts}
             />
           </DndProvider>
-          
         ) : (
-          // Modo de selección de ID
-          <IdSelector
-            databaseIds={databaseIds}
-            onIdSelect={handleIdSelect}
-          />
+          <IdSelector databaseIds={databaseIds} onIdSelect={setSelectedId} />
         )}
       </div>
     </>
