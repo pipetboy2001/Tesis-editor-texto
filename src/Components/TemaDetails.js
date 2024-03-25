68405c3b68a10c9d35e1279a91349c5f364686da
Pipetboy <felipefelizx@gmail.com>
2024-03-24 21:36:13 -0300

    Feature - Incluir en acordeon

--- src/Components/TemaDetails.js
+++ src/Components/TemaDetails.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
+import { Accordion, Card, Button } from 'react-bootstrap';
 import TrashIcon from "@atlaskit/icon/glyph/trash";
 import AddCircleIcon from "@atlaskit/icon/glyph/add";
 import EditIcon from "@atlaskit/icon/glyph/edit";
 import EditModal from "./EditModal";
 import "./../Styles/TemaDetails.css";
-
 const BASE_URL = "http://localhost:8000";
 
 const TemaDetails = ({ tema, selectedText }) => {
@@ -24,7 +23,7 @@ const TemaDetails = ({ tema, selectedText }) => {
 
   useEffect(() => {
     let intervalId; // Declarar el intervalId fuera del alcance de useEffect
-    
+
     const fetchData = async () => {
       const endpoint = `${BASE_URL}/text/temas/${idText}/${idTema}`;
       try {
@@ -42,22 +41,22 @@ const TemaDetails = ({ tema, selectedText }) => {
         console.error("Error al obtener datos:", error.message);
       }
     };
-    
+
     const startDataFetching = () => {
       fetchData(); // Llamar a fetchData inmediatamente al iniciar
       intervalId = setInterval(fetchData, 1000); // Establecer intervalo para llamar a fetchData cada 1000ms
     };
-    
+
     const stopDataFetching = () => {
       clearInterval(intervalId); // Detener la actualización automática de datos
     };
-  
+
     if (!editOrderMode) {
       startDataFetching(); // Iniciar la actualización automática de datos solo si no estás en modo de edición de orden
     } else {
       stopDataFetching(); // Detener la actualización automática de datos si estás en modo de edición de orden
     }
-  
+
     return () => clearInterval(intervalId); // Limpiar el intervalo al desmontar el componente
   }, [idText, idTema, editOrderMode]); // Dependencia editOrderMode agregada al arreglo de dependencias
 
@@ -80,7 +79,7 @@ const TemaDetails = ({ tema, selectedText }) => {
           elemento.orden = index + 1;
         });
         setElementos(updatedElementos);
-  
+
         // Envía una solicitud para actualizar los valores de orden en la base de datos
         await Promise.all(updatedElementos.map(async (elemento) => {
           await fetch(`${BASE_URL}/text/temas/${idText}/${idTema}/${elemento._id}`, {
@@ -94,7 +93,7 @@ const TemaDetails = ({ tema, selectedText }) => {
       console.error("Error al eliminar el elemento:", error.message);
     }
   };
-  
+
   const handleEditElement = (elemento) => {
     setEditingElemento(elemento); // Al hacer clic en "Editar", establecemos el elemento a editar en el estado
     setShowEditModal(true); // Abrimos el modal de edición
@@ -187,77 +186,75 @@ const TemaDetails = ({ tema, selectedText }) => {
     const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     return formattedDate;
   };
-  
+
   return (
     <div className="tema-details-container">
       <div className="tema-container row">
-        <div className="col-md-6">
-          <h3>Tema {tema.ordenTema}: {tema.tema}</h3>
-        </div>
-        <div className="col-md-6">
-          <Button variant="outline-primary" className="edit-button" onClick={handleAddElement} style={{ backgroundColor: "#0052CC", borderColor: "#0052CC" }}>
-            <AddCircleIcon size="small" /> Añadir Elemento
-          </Button>
-          <Button variant="danger" className="delete-button" onClick={handleDeleteTema} style={{ backgroundColor: "#ff5630", borderColor: "#ff5630" }}>
-            <TrashIcon size="small" /> Borrar Tema
-          </Button>
-          {!editOrderMode && (
-            <Button variant="primary" className="edit-order-button" onClick={handleEditOrder}>Editar Orden</Button>
-          )}
-          {editOrderMode && (
-            <Button variant="success" className="save-order-button" onClick={handleSaveOrder}>Guardar Orden</Button>
-          )}
-        </div>
-      </div>
-      <div className="tema-informacion">
-        {elementos.map((elemento, index) => (
-          <div key={elemento._id} className="elemento-container">
-            <div className="row">
-              <div className="col-md-6 col-sm-12">
-                <div className="d-flex align-items-center"> {/* Contenedor para alinear horizontalmente los elementos */}
-                  <div className="orden-circle mr-3">{editOrderMode ? <input type="number" value={elemento.orden} onChange={(e) => {
-                    const newOrden = parseInt(e.target.value);
-                    const updatedElementos = [...elementos];
-                    updatedElementos[index].orden = newOrden;
-                    setElementos(updatedElementos);
-                  }} /> : elemento.orden}</div> {/* Coloca el valor dentro del círculo */}
-                  <Button variant="primary" className="edit-button mr-3" onClick={() => handleEditElement(elemento)}>
-                    <EditIcon size="small" /> Editar
-                  </Button>
-                  <Button variant="danger" className="delete-button" onClick={() => handleDeleteElement(elemento._id)}>
-                    <TrashIcon size="small" /> Eliminar
-                  </Button>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="1">
+            <Accordion.Header><h3>Tema {tema.ordenTema}: {tema.tema}</h3></Accordion.Header>
+            <Accordion.Body>
+
+              <div className="col-md-6">
+                <Button variant="outline-primary" className="edit-button" onClick={handleAddElement} style={{ backgroundColor: "#0052CC", borderColor: "#0052CC" }}>
+                  <AddCircleIcon size="small" /> Añadir Elemento
+                </Button>
+                <Button variant="danger" className="delete-button" onClick={handleDeleteTema} style={{ backgroundColor: "#ff5630", borderColor: "#ff5630" }}>
+                  <TrashIcon size="small" /> Borrar Tema
+                </Button>
+                {!editOrderMode && (
+                  <Button variant="primary" className="edit-order-button" onClick={handleEditOrder}>Editar Orden</Button>
+                )}
+                {editOrderMode && (
+                  <Button variant="success" className="save-order-button" onClick={handleSaveOrder}>Guardar Orden</Button>
+                )}
+              </div>
+              <div className="tema-informacion">
+                {elementos.map((elemento, index) => (
+                  <div key={elemento._id} className="elemento-container">
+                    <div className="row">
+                      <div className="col-md-6 col-sm-12">
+                        <div className="d-flex align-items-center"> {/* Contenedor para alinear horizontalmente los elementos */}
+                          <div className="orden-circle mr-3">{editOrderMode ? <input type="number" value={elemento.orden} onChange={(e) => {
+                            const newOrden = parseInt(e.target.value);
+                            const updatedElementos = [...elementos];
+                            updatedElementos[index].orden = newOrden;
+                            setElementos(updatedElementos);
+                          }} /> : elemento.orden}</div> {/* Coloca el valor dentro del círculo */}
+                          <Button variant="primary" className="edit-button mr-3" onClick={() => handleEditElement(elemento)}>
+                            <EditIcon size="small" /> Editar
+                          </Button>
+                          <Button variant="danger" className="delete-button" onClick={() => handleDeleteElement(elemento._id)}>
+                            <TrashIcon size="small" /> Eliminar
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+
+                    <div className="">
+                        <p><strong>{elemento.autor}</strong> {elemento.contenido} , {handleFormatDate(elemento.date)} | {elemento.tipo} - {elemento.sentimiento}</p>
+                  </div>
+                  </div>
                 </div>
+                ))}
               </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 col-sm-12">
-                <p><strong>Autor: </strong>{elemento.autor}</p>
-                <p><strong>Tipo: </strong>{elemento.tipo}</p>
-              </div>
-              <div className="col-md-6 col-sm-12">
-                <p><strong>Sentimiento: </strong>{elemento.sentimiento}</p>
-                <p><strong>Fecha: </strong>{handleFormatDate(elemento.date)}</p>
-              </div>
-            </div>
-            <div className="row contenido-section">
-              <div className="col-md-12">
-                <p>{elemento.contenido}</p>
-              </div>
-            </div>
-          </div>
-        ))}
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+
+        </div>
+
+        {showEditModal && (
+          <EditModal
+            show={showEditModal}
+            handleClose={handleCloseEditModal}
+            elemento={editingElemento} // Pasar editingElemento en lugar de elemento
+            onSave={handleSaveElement} // Pasar handleSaveElement directamente
+          />
+        )}
       </div>
-      {showEditModal && (
-        <EditModal
-          show={showEditModal}
-          handleClose={handleCloseEditModal}
-          elemento={editingElemento} // Pasar editingElemento en lugar de elemento
-          onSave={handleSaveElement} // Pasar handleSaveElement directamente
-        />
-      )}
-    </div>
-  );
+      );
 };
 
-export default TemaDetails;
+      export default TemaDetails;
